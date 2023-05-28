9a42d1c6c987ee79508cb420778f9b7fb78c7e04
gello mendez <128371781+cherry0021@users.noreply.github.com>
2023-05-27 23:38:55 -0700

    Update routes.py

--- apps/authentication/routes.py
+++ apps/authentication/routes.py
@@ -175,4 +175,27 @@ def gate1():
     print(value)
     return f"{value}  =>  {message}" 
     # Render the form template for GET requests
-
+    
+    
+@blueprint.route('/gate2', methods=['POST'])
+def gate2():
+#     gate = ['gate1', 'gate2', 'gate3', 'gate4', 'gate5', 'gate6', 'gate7', 'gate8', 'gate9']   
+#     gate = random.choice(gate)
+    print(gate)
+    value = request.form.get('value')
+    reqUrl = f"https://fdata1.up.railway.app/runserver/"
+    headersList = {
+    "Accept": "*/*",
+    "User-Agent": "Thunder Client (https://www.thunderclient.com)",
+    "Content-Type": "application/json" 
+    }
+    
+    payload = json.dumps({"card": value})
+    response = requests.request("POST", reqUrl, data=payload,  headers=headersList)
+    time.sleep(1)
+    message = find_between(response.text, '"message":"', '"')
+    # message = response.json()['message']
+    print(response.text)
+    print(value)
+    return f"{value}  =>  {message}" 
+    # Render the form template for GET requests
