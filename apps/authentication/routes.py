4cbe23802fb573fb3217b95ec8c3767469213d06
gello mendez <128371781+cherry0021@users.noreply.github.com>
2023-06-22 11:13:12 +0800

    Update routes.py

--- apps/authentication/routes.py
+++ apps/authentication/routes.py
@@ -183,7 +183,7 @@ def gate2():
 #     gate = random.choice(gate)
 #     print(gate)
     value = request.form.get('value')
-    reqUrl = "https://cvv-fdata1.up.railway.app/runserver/"
+    reqUrl = "https://ccn-fdata1.up.railway.app/runserver/"
     headersList = {
     "Accept": "*/*",
     "User-Agent": "Thunder Client (https://www.thunderclient.com)",
