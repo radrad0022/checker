99f9f33cf77826dab453af8dd9dc4a306a93fdcd
gello mendez <128371781+cherry0021@users.noreply.github.com>
2023-06-27 13:49:37 +0800

    Update run.py

--- run.py
+++ run.py
@@ -13,7 +13,7 @@ from apps import create_app, db
 
 # WARNING: Don't run with debug turned on in production!
 DEBUG = (os.getenv('DEBUG', 'False') == 'True')
-
+gate_list = os.environ.get('gate_list')
 # The configuration
 get_config_mode = 'Debug' if DEBUG else 'Production'
 
